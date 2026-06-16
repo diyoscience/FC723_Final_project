314a58ebc73380032ae2c4a850b8aa3adae803fe
Diyorbek <diyorbekjon0078@gmail.com>
2026-06-16 16:36:56 +0100

    Update main.py
    The main SeatSelection class added. This provides the feature to select seats based on several priorities and identify their status.

--- main.py
+++ main.py
@@ -29,17 +29,80 @@ class Seats:
                 f"{seat:2}"
                 f" {seats[f'{seat}A']}     {seats[f'{seat}B']}     {seats[f'{seat}C']}     X     {seats[f'{seat}D']}     {seats[f'{seat}E']}     {seats[f'{seat}F']} "
             )
+
+#class for choosing the seats according to preference
+class SeatSelection:
+    def __init__(self,seats):
+        self.seats=seats #loading the seat
+    #define function that will show the seats located near the window
+    def window_seats(self):
+        print(" WINDOWS SEATS seats")
+        print(" A     F")
+        print(" -------")
+        for seat in range(1, 81):
+            print(
+                f"{seat:2}"
+                f" {self.seats[f'{seat}A']}     {self.seats[f'{seat}F']}"
+            )
+    #define fucntion that will show seats located in the middle
+    def aisle_adjacent_seats(self):
+        print("AISLE ADJACENT SEATS")
+        print(" C     D")
+        print(" -------")
+        for seat in range(1, 81):
+            print(
+                f"{seat:2}"
+                f" {self.seats[f'{seat}C']}     {self.seats[f'{seat}D']}"
+            )
+    #define function for showing the seats in the chosen row
+    def row_seats(self,row):
+        print(f"Seats in {row} row")
+        print(
+            f"A:{self.seats[f'{row}A']} "
+            f"B:{self.seats[f'{row}B']} "
+            f"C:{self.seats[f'{row}C']} "
+            f"D:{self.seats[f'{row}D']} "
+            f"E:{self.seats[f'{row}E']} "
+            f"F:{self.seats[f'{row}F']}"
+        )
+   #define function that will show the interface of searching among seats
+    def priority_searching(self):
+        print(
+            "1. Search Windows seats \n",
+            "2. Search Aisle adjacent seats \n",
+            "3. Search row seats \n",
+        )
+        search = input("Enter your choice: ")
+        if search == "1":
+            self.window_seats()
+        elif search == "2":
+            self.aisle_adjacent_seats()
+        elif search == "3":
+            self.row_seats(int(input("Enter your row number: ")))
+        else:
+            print("Enter a valid choice!")
+
+    #define function that will hold the interface of searching among seats
+    def interface_seats(self):
+        while True:
+            ask = input("Do you want to choose seats according to your priority? (y/n): ").lower().strip()
+            if ask == "y":
+                self.priority_searching()
+            else:
+                break
+
+
 #class for Booking and other operation
 class Booking:
     def __init__(self, seats):
-
         self.seats=seats #loading seats
         self.booked_seats = []
+        self.search = SeatSelection(self.seats.seats) #calling a class
    #define function that checks avaibility of seats
     def check_availability(self):
         print("Check seats availability")
-        self.seats.show_seats() #calling the function
-
+        self.search.interface_seats() #calling function 
+        self.seats.show_seats()  # calling the function
         seat_number = input("Enter seat number: ").strip().upper()
 
         if seat_number not in self.seats.seats: # if input is not correct, it will show invalidity
@@ -57,32 +120,39 @@ class Booking:
     def create_booking(self):
         print("WELCOME TO BOOKING PAGE:")
         print("You can select your seat by checking its availability from seats below")
-        self.seats.show_seats() #calling the function
-        seat_number=input("Enter your seat number: ").strip().upper()
-        if seat_number not in self.seats.seats: #checking for validity of seat number
-            print("Invalid seat number. Please try again.")
+        while True :
+            self.seats.show_seats()# calling the function
+            seat_number = input("Enter your seat number: ").strip().upper()
+            if seat_number not in self.seats.seats:  # checking for validity of seat number
+                print("Invalid seat number. Please try again.")
 
-        elif self.seats.seats[seat_number]=="S":
-            print("Please select another seat!")
-        elif self.seats.seats[seat_number]=="F":
-            print(f"Do you want to book {seat_number}?")
-            assure=input("Do you want to book another seat? (y/n): ")
-            if assure=="y":
-                self.seats.seats[seat_number]="R"
-                self.booked_seats.append(seat_number)
-                print("Booking successful!")
+            elif self.seats.seats[seat_number] == "S":
+                print("Please select another seat!")
+            elif self.seats.seats[seat_number] == "F":
+                assure = input(f"Do you want to book {seat_number} seat? (y/n): ").lower().strip()
+                if assure == "y":
+                    self.seats.seats[seat_number] = "R"
+                    self.booked_seats.append(seat_number)
+                    print("Booking successful!")
+                else:
+                    print("Booking process cancelled!")
             else:
-                print("Booking process cancelled!")
-        else:
-            print("Please select another seat!")
+                print("Please select another seat!")
+            con=input("Do you want to book another seat? (y/n): ").lower().strip()
+            if con=="y":
+                pass
+            else:
+                print("Thank you!")
+                break
+
     #define function that will free the booked seat
     def free_seat(self):
         print("FREEING SEAT!")
-        seat_number_f=input("Enter your seat number: ")
+        seat_number_f=input("Enter your seat number: ").strip().upper()
         if seat_number_f not in self.seats.seats:
             print("Invalid seat number. Please try again.")
         elif self.seats.seats[seat_number_f]=="R":
-            assurance=input(f"Do you want to free {seat_number_f} seat? (y/n): ")
+            assurance=input(f"Do you want to free {seat_number_f} seat? (y/n): ").lower().strip()
             if assurance=="y":
                 self.seats.seats[seat_number_f] = "F" #freeing the seat
                 self.booked_seats.remove(seat_number_f) #freeing from the booked list
@@ -98,7 +168,6 @@ class Booking:
         print("Booked seats:")
         for spot in self.booked_seats:
             print(f"{spot}")
-
 
 
 
